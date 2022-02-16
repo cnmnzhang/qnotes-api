e42179b818ed0a79f78aa16c4aaf6b022f934478
czhan117 <59889383+czhan117@users.noreply.github.com>
2022-02-16 11:57:54 -0500

    implemented methods

--- server/data/NoteDao.js
+++ server/data/NoteDao.js
@@ -5,23 +5,45 @@ class NoteDao {
     constructor() {
         this.notes = [];
     }
-    async create ({ title, text }) {
 
+    async create ({ title, text }) {
+        const note = new Note(title, text);
+        this.notes.push(note);
+        return note;
     }
 
     async update (id, { title, text }) {
+        const index = this.notes.findIndex((note) => note._id === id);
+
+        if (title !== undefined) {
+            this.notes[index].title = title;
+        }
+
+        if (text !== undefined) {
+            this.notes[index].text = text;
+        }
         
+        return this.notes[index];
     }
 
     async delete (id) {
-        
+        const index = this.notes.findIndex((note) => note._id === id);
+        const note = this.notes[index];
+        this.notes.splice(index, 1);
+        return note;
     }
 
     async read (id) {
-        
+        return this.notes.find((note) => note._id === id);
     }
 
     async readAll (query = "") {
+        if (query !== "") {
+            return this.note.filter(
+                (note) => note.title.includes(query) || note.text.includes(query)
+            );
+        }
+        return this.notes
         
     }
 
