74e9828d44fea43c17f7f7549d5ef3d18eb568e0
czhan117 <59889383+czhan117@users.noreply.github.com>
2022-02-17 14:08:21 -0500

    accounted for pre-conditions

--- server/data/NoteDao.js
+++ server/data/NoteDao.js
@@ -1,4 +1,5 @@
 const Note = require("../model/Node");
+const ApiError = require("../model/ApiError")
 
 // Data access object
 class NoteDao {
@@ -6,14 +7,25 @@ class NoteDao {
         this.notes = [];
     }
 
-    async create ({ title, text }) {
+    async create({ title, text }) {
+        if (title === undefined || title === "") {
+            throw new ApiError(400, "Every note must have a none-empty title!");
+        }
+
+        if (text === undefined) {
+            throw new ApiError(400, "Every note must have a text attribute!");
+        }
         const note = new Note(title, text);
         this.notes.push(note);
         return note;
     }
 
-    async update (id, { title, text }) {
+    async update(id, { title, text }) {
         const index = this.notes.findIndex((note) => note._id === id);
+
+        if (index === -1) {
+            throw new ApiError(404, "There is no note with the given ID!");
+        }
 
         if (title !== undefined) {
             this.notes[index].title = title;
@@ -22,32 +34,37 @@ class NoteDao {
         if (text !== undefined) {
             this.notes[index].text = text;
         }
-        
+
         return this.notes[index];
     }
 
-    async delete (id) {
+    async delete(id) {
         const index = this.notes.findIndex((note) => note._id === id);
+
+        if (index === -1) {
+            throw new ApiError(404, "There is no note with the given ID!");
+        }
+        
         const note = this.notes[index];
         this.notes.splice(index, 1);
         return note;
     }
 
-    async read (id) {
+    async read(id) {
         return this.notes.find((note) => note._id === id);
     }
 
-    async readAll (query = "") {
+    async readAll(query = "") {
         if (query !== "") {
             return this.note.filter(
                 (note) => note.title.includes(query) || note.text.includes(query)
             );
         }
         return this.notes
-        
+
     }
 
-    
+
 }
 
 module.exports = NoteDao
